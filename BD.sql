9a11690b3375c0ba90789072f1a41ce1a58c9eec
MeFFoS <meffos@list.ru>
2018-01-05 02:19:06 +0400

    add database

---
+++ BD.sql
@@ -0,0 +1,28 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2017-12-30 01:03:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for accounts
+-- ----------------------------
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(24) DEFAULT NULL,
+  `password` varchar(36) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
